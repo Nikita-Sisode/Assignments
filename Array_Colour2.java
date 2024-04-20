4b0a3171890ac5b745fbc6acc847caa4e84cf6c6
Nikita-Sisode <161593597+Nikita-Sisode@users.noreply.github.com>
2024-04-20 18:32:13 +0530

    Add files via upload

---
+++ Array_Colour2.java
@@ -0,0 +1,18 @@
+/*1.	Write a Java program to create a new array list, add some colors (string) and print out the collection.*/
+
+
+
+import java.util.*;
+public class Array_Colour2 {
+  public static void main(String[] args) {
+  List<String> list_Strings = new ArrayList<String>();
+  list_Strings.add("Green");
+  list_Strings.add("Yellow");
+  list_Strings.add("Black");
+  list_Strings.add("Red");
+  list_Strings.add("White");
+  list_Strings.add("Pink");
+  list_Strings.add("Blue");
+  System.out.println(list_Strings);
+ }
+}
